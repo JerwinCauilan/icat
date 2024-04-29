81c470ff921fdcc4385c7b3b57b2fb4eccdbfcf5
Jerwin Cauilan <114655842+JerwinCauilan@users.noreply.github.com>
2024-04-30 01:30:44 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,11 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://icat-93eb7.web.app"],
+  methods: ["POST","GET", "DELETE","PUT"],
+  credentials: true
+}));
 
 
 // EXPRESS MIDDLEWARE 
